05d0cca07abb170544e77a95c5a94d7f06ea4364
Garrett Harty <gharty@live.com>
2021-08-20 18:32:46 -0500

    setup store instance and two reducers for storing data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import Header from '../Header/Header';
+import { useSelector } from 'react-redux';
 
 function App() {
+  // just setting these up here to make sure the store instance
+  // is initialized okay
+  // logs should show default empty arrays at this point
+  const responseReducer = useSelector(store => store.responseReducer);
+  const resultReducer = useSelector(store => store.resultReducer);
+  console.log(responseReducer);
+  console.log(resultReducer);
 
   return (
     <div className='App'>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const responseReducer = (state = [], action) => {
+    return state;
+};
+
+const resultReducer = (state = [], action) => {
+    return state;
+};
+
+const storeInstance = createStore(
+    combineReducers({
+        responseReducer,
+        resultReducer,
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
